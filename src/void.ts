238255b426e2746553930eaa6b0202d1ace61c00
Ryota Maeda <r.maeda@jaythree.com>
2022-05-27 03:04:09 +0900

    create void.ts

---
+++ src/void.ts
@@ -0,0 +1,25 @@
+export {};
+
+// boid型は、何もデータが存在しないときに使用する(関数にアノテーションを指定する)
+
+// ①空の関数（undefinedの戻り値が必要な処理の場合）
+function returnNothing(): undefined {
+
+	console.log("I dont't return anything!");
+	// 空の関数の場合でも、空（undefined）を返さなければならないため、
+	// 下記のいずれか一つを記述する必要がある
+	//  return;
+	 return undefined;
+}
+console.log(returnNothing());
+
+
+
+// ②空の関数（戻り値を必要としていない場合）
+function returnNothing2(): void {
+
+	console.log("I don't return anything");
+
+}
+
+console.log(returnNothing2());
