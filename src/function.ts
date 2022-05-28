c09b5672a590e38b2ffa616a71e1d09031e5b526
Ryota Maeda <r.maeda@jaythree.com>
2022-05-28 19:38:54 +0900

    create function.ts

---
+++ src/function.ts
@@ -0,0 +1,14 @@
+export {};
+
+// functionキーワード（注意点：引数と戻り値）
+
+
+// heightとweightは数字が入るのでnumber型
+// 戻り値つまり、bmiも数字で戻るのでnumber型でさる。
+// 下記のようにテキストを加える場合は、string型となる
+function bmi (height:number, weight:number):string {
+	// return weight / (height * height) +;
+	return "BMIは" + weight / (height * height) + "です";
+}
+
+console.log(bmi(1.69, 73));
